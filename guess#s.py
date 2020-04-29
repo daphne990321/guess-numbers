c1375e37577cb167cf7f42232bc6764108f3b5c3
Daphne Huang <daphne990321@gmail.com>
2020-04-29 21:02:13 +0800

    decision

--- guess#s.py
+++ guess#s.py
@@ -1,12 +1,16 @@
 #guess#s
 import random
-r = random.randint(1,100)
+a = input('請輸入你想要的下界：')
+a = int(a)
+b = input('請輸入你想要的上界：')
+b = int(b)
+r = random.randint(a,b)
 count = 0
 
 while True:
 
 	count += 1 #就是count = count + 1的意思
-	a = input('請猜一個數字')
+	a = input('請猜一個數字：')
 	a = int(a)
 
 	if a == r:
