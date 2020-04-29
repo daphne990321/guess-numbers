64950134fa00d1a1471adb6b5bf631bfef97a26c
Daphne Huang <daphne990321@gmail.com>
2020-04-29 20:54:24 +0800

    count v

--- guess#s.py
+++ guess#s.py
@@ -1,18 +1,20 @@
 #guess#s
 import random
 r = random.randint(1,100)
+count = 0
 
 while True:
 
+	count += 1 #就是count = count + 1的意思
 	a = input('請猜一個數字')
 	a = int(a)
 
 	if a == r:
-		print("終於猜對了")
+		print("終於猜對了，恭喜你總共猜了", count, '次')
 		break
 
 	elif a > r:
-		print('再小')
+		print("再小，你猜了第", count, "次")
 
 	elif a < r:
-		print('再大')
+		print('再大，你猜了第', count, '次')
